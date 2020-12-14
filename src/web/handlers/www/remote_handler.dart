ce5e1c66f75833a27c208af03448584ff1f1e247
Krzysztof Hycnar <khycnar@o2.pl>
2020-12-14 16:24:41 +0000

    - Dodatkowe uproszczenia w kodzie

--- src/web/handlers/www/remote_handler.dart
+++ src/web/handlers/www/remote_handler.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
 
-import 'package:tuple/tuple.dart';
-
 import '../web_request_handler.dart';
 
 class RemoteHandler extends WebRequestHandler{
@@ -17,17 +15,23 @@ class RemoteHandler extends WebRequestHandler{
     return res.render('remote', {
       'title' : 'Webowy pilot zdalnego sterowania',
       'buttons' : {
-        'web-button-0' : _buttonJson('digit-0', Tuple3('Start/Stop','', false)),
-        'web-button-1' : _buttonJson('digit-1', Tuple3('Następny Cue Punkt',
-            'Skacze do następnego cue punktu lub przewija 75% ekranu w górę, jeśli w skrypcie nie ma zdefiniowanych cue punktów', true)),
-        'web-button-2' : _buttonJson('digit-2', Tuple3('Poprzedni Cue Punkt',
-            'Skacze do poprzedniego cue punktu lub przewija 75% ekranu w dół, jeśli w skrypcie nie ma zdefiniowanych cue punktów', true)),
-        'web-button-3' : _buttonJson('digit-3', Tuple3('Zwiększ Prędkość',
-            'Zwiększa prędkość o jeden punkt, albo przewija 10% ekranu w górę jeśli skrypt nie jest aktualnie odtwarzany', true)),
-        'web-button-4' : _buttonJson('digit-4', Tuple3('Zmniejsz Prędkość',
-            'Zmniejsza prędkość o jeden punkt, albo przewija 10% ekranu w dół jeśli skrypt nie jest aktualnie odtwarzany', true)),
-        'web-button-5' : _buttonJson('digit-5', Tuple3('Skocz na Początek', '', false)),
-        'web-button-6' : _buttonJson('digit-6', Tuple3('Skocz na Koniec', '', false)),
+        'web-button-0' : _buttonJson('digit-0', 'Start/Stop','', false),
+
+        'web-button-1' : _buttonJson('digit-1', 'Następny Cue Punkt',
+            'Skacze do następnego cue punktu lub przewija 75% ekranu w górę, jeśli w skrypcie nie ma zdefiniowanych cue punktów', true),
+
+        'web-button-2' : _buttonJson('digit-2', 'Poprzedni Cue Punkt',
+            'Skacze do poprzedniego cue punktu lub przewija 75% ekranu w dół, jeśli w skrypcie nie ma zdefiniowanych cue punktów', true),
+
+        'web-button-3' : _buttonJson('digit-3', 'Zwiększ Prędkość',
+            'Zwiększa prędkość o jeden punkt, albo przewija 10% ekranu w górę jeśli skrypt nie jest aktualnie odtwarzany', true),
+
+        'web-button-4' : _buttonJson('digit-4', 'Zmniejsz Prędkość',
+            'Zmniejsza prędkość o jeden punkt, albo przewija 10% ekranu w dół jeśli skrypt nie jest aktualnie odtwarzany', true),
+
+        'web-button-5' : _buttonJson('digit-5', 'Skocz na Początek', '', false),
+
+        'web-button-6' : _buttonJson('digit-6', 'Skocz na Koniec', '', false),
       },
       'scrollers': {
         'seek' : {
@@ -46,11 +50,11 @@ class RemoteHandler extends WebRequestHandler{
     });
   }
 
-  Map<String, dynamic> _buttonJson(String action, Tuple3<String, String, bool> info) => {
+  Map<String, dynamic> _buttonJson(String action, String label, String tooltip, bool hasTooltip) => {
       'action': action,
-      'label': info.item1,
-      'tooltip': info.item2,
-      'hasTooltip': info.item3,
+      'label': label,
+      'tooltip': tooltip,
+      'hasTooltip': hasTooltip,
     };
 }
 
